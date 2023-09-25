89c9c83d189eb90e0da49549bb03c07298ff370f
hswinata <harrysurywinata@gmail.com>
2023-09-25 10:42:21 +0200

    Refactor library.js

--- library.js
+++ library.js
@@ -1,130 +1,116 @@
+// Define the Book class
+class Book {
+  constructor(title, author, pages, hasRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.hasRead = hasRead;
+  }
+
+  toggleRead() {
+    this.hasRead = !this.hasRead;
+  }
+}
+
+// Define a library array to store books
 const myLibrary = [];
 
-// Book class definition
-class Book {
-  constructor(_title, _author, _pages, _hasRead) {
-    this.title = _title;
-    this.author = _author;
-    this.pages = _pages;
-    this.hasRead = _hasRead;
+// DOM elements
+const bookContainer = document.querySelector(".book-container");
+const buttonNew = document.querySelector(".button-new");
+const buttonClose = document.querySelector(".button-close");
+const form = document.querySelector("form");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector("#overlay");
+
+// Event listeners
+buttonNew.addEventListener("click", activateModal);
+buttonClose.addEventListener("click", closeModal);
+form.addEventListener("submit", handleFormSubmit);
+
+// Event delegation for card clicks
+bookContainer.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("eyeopen-icon") ||
+    e.target.classList.contains("eyeslash-icon")
+  ) {
+    const cardIndex = e.target.closest(".card").getAttribute("data-index");
+    myLibrary[cardIndex].toggleRead();
+    renderLibrary();
+  } else if (e.target.classList.contains("trash-icon")) {
+    const cardIndex = e.target.closest(".card").getAttribute("data-index");
+    removeBook(cardIndex);
   }
+});
 
-  set toggleRead(_hasRead) {
-    return (this.hasRead = _hasRead);
-  }
-}
-
-function toggleCard() {
-  let items = bookContainer.querySelectorAll(".card");
-  items.forEach((item) => {
-    let indexNumber = item.getAttribute("data-index");
-    item.addEventListener("click", (e) => {
-      if (e.target.classList.contains("eyeopen-icon")) {
-        e.target.classList.remove("eyeopen-icon");
-        e.target.classList.toggle("eyeslash-icon");
-        e.target.setAttribute("src", "images/eye-slash.svg");
-        e.target.setAttribute("title", `I haven't read it!`);
-        myLibrary[indexNumber].toggleRead = 0;
-      } else if (e.target.classList.contains("eyeslash-icon")) {
-        e.target.classList.remove("eyeslash-icon");
-        e.target.classList.toggle("eyeopen-icon");
-        e.target.setAttribute("src", "images/eye-open.svg");
-        e.target.setAttribute("title", `I've read it!`);
-        myLibrary[indexNumber].toggleRead = 1;
-      }
-    });
-  });
-}
-
-function deleteCard() {
-  let items = bookContainer.querySelectorAll(".card");
-  items.forEach((item) => {
-    item.addEventListener("click", (e) => {
-      if (e.target.classList.contains("trash-icon")) {
-        let cardIndex = item.getAttribute("data-index");
-        myLibrary.splice(cardIndex, 1);
-        item.remove();
-      }
-    });
-  });
-}
-
-let bookContainer = document.querySelector(".book-container");
-let buttonNew = document.querySelector(".button-new");
-buttonNew.addEventListener("click", () => activateModal());
-
+// Function to activate the modal
 function activateModal() {
-  let modal = document.querySelector(".modal");
-  let overlay = document.querySelector("#overlay");
   modal.classList.add("active");
   overlay.classList.add("active");
 }
 
-let buttonClose = document.querySelector(".button-close");
-buttonClose.addEventListener("click", () => closeModal());
-
+// Function to close the modal
 function closeModal() {
-  let modal = document.querySelector(".modal");
-  let overlay = document.querySelector("#overlay");
   modal.classList.remove("active");
   overlay.classList.remove("active");
 }
 
-function addBookToLibrary() {
-  let _title = document.getElementById("title").value;
-  let _author = document.getElementById("author").value;
-  let _pages = document.getElementById("pages").value;
-  let _hasRead = document.getElementById("hasRead").value;
-  let _newBook = new Book(_title, _author, _pages, _hasRead);
-  myLibrary.push(_newBook);
-  render();
+// Function to handle form submission
+function handleFormSubmit(e) {
+  e.preventDefault();
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
+  const hasRead = document.getElementById("hasRead").value === "1";
+  addBookToLibrary(title, author, pages, hasRead);
+  form.reset();
+  closeModal();
 }
 
-function render() {
+// Function to add a new book to the library
+function addBookToLibrary(title, author, pages, hasRead) {
+  const newBook = new Book(title, author, pages, hasRead);
+  myLibrary.push(newBook);
+  renderLibrary();
+}
+
+// Function to remove a book from the library
+function removeBook(index) {
+  myLibrary.splice(index, 1);
+  renderLibrary();
+}
+
+// Function to render the library
+function renderLibrary() {
   bookContainer.innerHTML = "";
-  myLibrary.forEach((card, index) => {
-    let cardContainer = document.createElement("div");
-    bookContainer.append(cardContainer);
+  myLibrary.forEach((book, index) => {
+    const cardContainer = document.createElement("div");
     cardContainer.classList.add("card");
-
     cardContainer.setAttribute("data-index", index);
-
-    let sentence = `${card.title} by ${card.author}`;
-    cardContainer.append(sentence);
-
-    let actionIcons = document.createElement("div");
-    let trashIcon = document.createElement("img");
-    let eyeOpenIcon = document.createElement("img");
-    let eyeSlashIcon = document.createElement("img");
-
-    cardContainer.append(actionIcons);
+    const sentence = `${book.title} by ${book.author}`;
+    cardContainer.innerHTML = sentence;
+    const actionIcons = document.createElement("div");
     actionIcons.classList.add("action-icons");
-
-    if (card.hasRead == "1") {
-      actionIcons.append(eyeOpenIcon);
-      eyeOpenIcon.setAttribute("src", "images/eye-open.svg");
-      eyeOpenIcon.classList.add("eyeopen-icon");
-      eyeOpenIcon.setAttribute("title", `I've read it!`);
-    } else if (card.hasRead == "0") {
-      actionIcons.append(eyeSlashIcon);
-      eyeSlashIcon.setAttribute("src", "images/eye-slash.svg");
-      eyeSlashIcon.classList.add("eyeslash-icon");
-      eyeSlashIcon.setAttribute("title", `I haven't read it!`);
-    }
-
-    actionIcons.append(trashIcon);
-    trashIcon.classList.add("trash-icon");
+    const eyeIcon = document.createElement("img");
+    eyeIcon.setAttribute(
+      "src",
+      book.hasRead ? "images/eye-open.svg" : "images/eye-slash.svg"
+    );
+    eyeIcon.classList.add(book.hasRead ? "eyeopen-icon" : "eyeslash-icon");
+    eyeIcon.setAttribute(
+      "title",
+      book.hasRead ? `I've read it!` : `I haven't read it!`
+    );
+    const trashIcon = document.createElement("img");
     trashIcon.setAttribute("src", "images/trash.svg");
+    trashIcon.classList.add("trash-icon");
     trashIcon.setAttribute("title", "Delete");
+    actionIcons.appendChild(eyeIcon);
+    actionIcons.appendChild(trashIcon);
+    cardContainer.appendChild(actionIcons);
+    bookContainer.appendChild(cardContainer);
   });
 }
 
-let form = document.querySelector("form");
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  addBookToLibrary();
-  form.reset();
-  closeModal();
-  deleteCard();
-  toggleCard();
-});
+// Initial rendering
+renderLibrary();
